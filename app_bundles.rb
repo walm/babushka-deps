f49a8e5063cdbb34819130f39fd93e11ffa483df
Andreas Wålm <walan@walmingair.walm.net>
2010-10-29 16:31:43 +0200

    Hey, my first deps

---
+++ app_bundles.rb
@@ -0,0 +1,15 @@
+dep 'LaunchBar.app' do
+  source 'http://www.obdev.at/downloads/launchbar/LaunchBar-5.0.3.dmg'
+end
+
+dep 'Fluid.app' do
+  source 'http://fluidapp.com/dist/Fluid_0.9.6.zip'
+end
+
+dep 'Skype.app' do
+  source 'http://download.skype.com/macosx/Skype_2.8.0.851.dmg'
+end
+
+dep 'Transmit.app' do
+  source 'http://www.panic.com/transmit/d/Transmit 4.0.zip'
+end
